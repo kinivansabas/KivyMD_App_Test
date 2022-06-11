4b1693d5401fd4769e2e5629d63b3ecbfbe0d7da
kinivansabas <ivansabaskin409@gmail.com>
2022-06-11 16:38:14 +0300

    Auto build APK file

--- main.py
+++ main.py
@@ -1,10 +1,171 @@
+
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ListProperty
+
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
+
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.icon_definitions import md_icons
+from kivymd.font_definitions import fonts
+
+KV = '''
+# Menu item in the DrawerList list.
+<ItemDrawer>:
+    theme_text_color: "Custom"
+    on_release: self.parent.set_color_item(self)
+
+    IconLeftWidget:
+        id: icon
+        icon: root.icon
+        theme_text_color: "Custom"
+        text_color: root.text_color
 
 
-class MainApp(MDApp):
+<ContentNavigationDrawer>:
+    orientation: "vertical"
+    padding: "8dp"
+    spacing: "8dp"
+
+    AnchorLayout:
+        anchor_x: "left"
+        size_hint_y: None
+        height: avatar.height
+
+        Image:
+            id: avatar
+            size_hint: None, None
+            size: "56dp", "56dp"
+            source: "data/logo/kivy-icon-256.png"
+            
+    MDLabel:
+        text: app.title
+        font_style: "Button"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+    MDLabel:
+        text: app.by_who
+        font_style: "Caption"
+        size_hint_y: None
+        height: self.texture_size[1]
+
+
+    ScrollView:
+
+        DrawerList:
+            id: md_list
+
+
+
+Screen:
+
+    MDNavigationLayout:
+
+        ScreenManager:
+
+            Screen:
+
+                BoxLayout:
+                    orientation: 'vertical'
+
+                    MDToolbar:
+                        title: app.title
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
+                        right_action_items: [['star-outline', lambda x: app.on_star_click()]]
+                        md_bg_collor: 0.2, 0, 0, 0.1
+
+                    MDTabs:
+                        id: tabs
+                        on_tab_switch: app.on_tab_switch(*args)
+                        size_hint_y: None
+                        height: "48dp"
+                        tab_indicator_anim: False
+                        background_color: 0.1, 0.1, 0.1, 1
+                        
+                    Widget:
+
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                id: content_drawer
+'''
+
+
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
+
+
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
+    text_color = ListProperty((0, 0, 0, 1))
+
+
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
+
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.primary_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.primary_color
+
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
+
+class Electrobez_Test(MDApp):
+    title = 'Тесты по электробезопасности'
+    by_who = 'by Ivan Shabashkin'
+
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        return Builder.load_string(KV)
 
+    def on_start(self):
+        icons_item_menu_lines = {
+            "star": "Оценить",
+            "github": "Исходный код",
+            "share-variant": "Отправить приложение",
+            "shield-sun": "Тёмная/Светлая тема",
+        }
+        icons_item_menu_tabs = {
+            "abacus": "Тесты",
+            "account-question": "Информация",
+        }
+        for icon_name in icons_item_menu_lines.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item_menu_lines[icon_name])
+            )
+       # for name_tab in list(md_icons.keys())[15:30]:
+           # self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
 
-MainApp().run()
+        for icon_name, name_tab in icons_item_menu_tabs.items():
+            self.root.ids.tabs.add_widget(
+                Tab( icon=icon_name, title =f"[ref={name_tab}][font={fonts[-1]['fn_regular']}]{icons_item_menu_tabs[icon_name]}[/font][/ref]{name_tab}")
+            )
+
+    def on_tab_switch(
+            self, instance_tabs, instance_tab, instance_tab_label, tab_text
+    ):
+        '''Called when switching tabs.
+
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+
+        print("tab clicked! " + tab_text)
+
+    def on_star_click(self):
+        pass
+
+Electrobez_Test().run()
